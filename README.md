92957eadf7ae0387cee43e9fd77f67e644970e34
snarkyboojum <snarkyboojum@gmail.com>
2020-06-08 17:38:31 +1000

    Initial commit - sketching out the high level implementation

---
+++ README.md
@@ -0,0 +1,8 @@
+## Overview
+
+An implementation of X25519 for use Elliptic Curve Diffie-Hellman (ECDH), written in Rust.
+
+## References
+
+1. [Elliptic Curves for Security](https://tools.ietf.org/html/rfc7748) - RFC 7748
+2. [SEC 1: Elliptic Curve Cryptography](http://www.secg.org/sec1-v2.pdf)
---
+++ src/lib.rs
@@ -0,0 +1,67 @@
+/*
+
+Curve25519 notes:
+
+p - the prime number of the underlying field
+GF(p) - finite field with p elements
+A - element in GF(p), but not -2 or 2
+d - a non-zero element of GF(p), not 1 (Edwards), and not -1 (twisted Edwards)
+order - order of the prime-order subgroup
+P - generator point over GF(p) of prime order
+...
+
+The prime 2^255 - 19 is ideal for performance.
+
+See References[1] from p 7 for X25519 implementation notes.
+
+*/
+
+fn encode_ucoord() {}
+
+fn decode_ucoord() {}
+
+fn decode_le() {}
+
+// decode 32 byte scalars for X25519
+fn decode_scalar(k: [u8; 32]) -> u64 {
+    0
+}
+
+fn x25519(k: [u8; 32], u: &[u8], v: &mut [u8]) {}
+
+#[cfg(test)]
+mod tests {
+    // tests taken straight from p11, https://tools.ietf.org/html/rfc7748#ref-curve25519
+    #[test]
+    fn test_x25519() {
+        use hex::FromHex;
+
+        let mut input_scalar =
+            Vec::from_hex("a546e36bf0527c9d3b16154b82465edd62144c0ac1fc5a18506a2244ba449ac4");
+        let mut input_ucoord =
+            Vec::from_hex("e6db6867583030db3594c1a424b15f7c726624ec26b3353b10a903a6d0ab1c4c");
+        let mut output_ucoord =
+            Vec::from_hex("c3da55379de9c6908e94ea4df28d084f32eccf03491c71f754b4075577a28552");
+
+        input_scalar =
+            Vec::from_hex("4b66e9d4d1b4673c5ad22691957d6af5c11b6421e0ea01d42ca4169e7918ba0d");
+        input_ucoord =
+            Vec::from_hex("e5210f12786811d3f4b7959d0538ae2c31dbe7106fc03c3efc4cd549c715a493");
+        output_ucoord =
+            Vec::from_hex("95cbde9476e8907d7aade45cb4b873f88b595a68799fa152e6f8f7647aac7957")
+    }
+
+    fn test_x25519_ktimes() {
+        use hex::FromHex;
+
+        let k = Vec::from_hex("0900000000000000000000000000000000000000000000000000000000000000");
+        let u = Vec::from_hex("0900000000000000000000000000000000000000000000000000000000000000");
+
+        for i in 1..1_000_000 {
+            if i == 0 {
+            } else if i == 1000 {
+            } else if i == 999_999 {
+            }
+        }
+    }
+}
